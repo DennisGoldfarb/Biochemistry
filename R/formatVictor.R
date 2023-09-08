37812cfbef8349a8af4fc5d919513c25f634b526
DennisGoldfarb <goldfarb.dennis@wustl.edu>
2023-09-08 14:14:30 -0500

    initial commit

--- R/formatVictor.R
+++ R/formatVictor.R
@@ -1,3 +1,12 @@
+#' Read florescence data from PerkinElmer Victor and format it
+#'
+#' @param path path to data file
+#'
+#' @return list of metadata and measurement table
+#' @export
+#'
+#' @examples
+#' formatVictor("~/Downloads/BCA.txt")
 formatVictor <- function(path)
 {
 
---
+++ tests/testthat/test-formatVictor.R
@@ -0,0 +1,3 @@
+test_that("formatVictor() reads an example file", {
+
+})
